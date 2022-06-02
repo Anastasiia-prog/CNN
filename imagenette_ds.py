6b789c40659b9e6c5fcae9ab99fd2de91d3794e0
Anastasiia-prog <anastasiia.41297@gmail.com>
2022-06-02 09:46:19 +0000

    Импорт imagenette датасета из папки
    Код из импортом imagenette320 из указанной папки, предполагается что скачивание данных из интернета уже было произведено

---
+++ imagenette_ds.py
@@ -0,0 +1,37 @@
+from torchvision import datasets
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms as T
+import torch
+    
+
+def load_imagenette(BATCH_SIZE: int=16, dir_name: str='./data/'):
+    
+    dataset_name = "imagenette2-320"
+
+    data_dir = dir_name + dataset_name
+    
+    transform_train = T.Compose([T.Resize((224, 224)), T.RandomHorizontalFlip(),
+                                 T.RandomRotation(45), T.ToTensor()])
+    transform_test = T.Compose([T.Resize((224, 224)), T.ToTensor()])
+
+    trainset = datasets.ImageFolder(data_dir+'/train', transform=transform_train)
+    trainset.classes = ['tench', 'English springer', 'cassette player', 'chain saw', 'church', 
+                        'French horn', 'garbage truck', 'gas pump', 'golf ball', 'parachute']
+
+    val_ds = datasets.ImageFolder(data_dir+'/val', transform=transform_test)
+    val_ds.classes = ['tench', 'English springer', 'cassette player', 'chain saw', 'church', 
+                        'French horn', 'garbage truck', 'gas pump', 'golf ball', 'parachute']
+
+    len_test, len_val = 1000, len(val_ds)-1000
+    test_dataset, valid_dataset = random_split(val_ds, [len_test, len_val])
+    
+    test_dataset.classes = ['tench', 'English springer', 'cassette player', 'chain saw', 'church', 
+                        'French horn', 'garbage truck', 'gas pump', 'golf ball', 'parachute']
+    valid_dataset.classes = ['tench', 'English springer', 'cassette player', 'chain saw', 'church', 
+                        'French horn', 'garbage truck', 'gas pump', 'golf ball', 'parachute']
+
+    train_loader = DataLoader(trainset, BATCH_SIZE, shuffle=True)
+    val_loader = DataLoader(valid_dataset, BATCH_SIZE)
+    test_loader = DataLoader(test_dataset, BATCH_SIZE)
+    
+    return train_loader, val_loader, test_loader, test_dataset
