bf7e1a2223c42cb06946eb3ef05cd54dc680b3b9
Anastasiia-prog <anastasiia.4127@gmail.com>
2022-07-12 15:09:15 +0000

    fixed segmentation predict

--- model_learning.py
+++ model_learning.py
@@ -1,15 +1,19 @@
+""" Training, validation and predictions for classification and segmentation tasks. """
+
 import matplotlib.pyplot as plt
 import torch
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score, jaccard_score
 import numpy as np
 import math
+import torchmetrics
 
 class Train:
-    
+    """ Train, validation and testing model for classification or
+    segmenatation task. Plot losses and metrics results.
+    """
     def __init__(self, model, loss_function, optimizer, num_epochs,
                  train_loader, val_loader, test_loader, test_dataset, device):
-        
         self.model = model
         self.num_epochs = num_epochs
         self.loss_function = loss_function
@@ -23,22 +27,23 @@ class Train:
         self.avg_loss_train = []
         self.avg_loss_valid = []
         self.accuracy_valid = []
-        
+        self.iou_valid = []
     
     def get_train_avg_loss(self) -> list:
-        # return saved average train loss
-        # for every epoch
+        """ Get saved average train loss for every epoch. """
         return self.avg_loss_train
     
     def get_valid_avg_loss(self) -> list:
-        # return saved average validation 
-        # loss for every epoch
+        """ Get saved average validation loss for every epoch. """
         return self.avg_loss_valid
     
     def get_valid_accuracy(self) -> list:
-        # return saved average validation
-        # accuracy for every epoch
+        """ Get saved average validation accuracy for every epoch. """
         return self.accuracy_valid
+    
+    def get_valid_iou(self) -> np.array:
+        """ Get saved average validation IoU score for every epoch. """
+        return self.iou_valid
     
     def training_one_epoch(self):
         train_losses = []
@@ -53,9 +58,9 @@ class Train:
             labels = labels.to(self.device)
 
             self.optimizer.zero_grad()
-
-            output = self.model(inps)
-            loss = self.loss_function(output, labels)
+            
+            outputs = self.model(inps)
+            loss = self.loss_function(outputs, labels)
             loss.backward()
             self.optimizer.step()
 
@@ -67,61 +72,67 @@ class Train:
         return avg_loss
     
     def val_segmentation_one_epoch(self):
-        
         running_loss = 0.
-        self.model.train(False)
-        with torch.no_grad():
-            total = 0.
-            correct = 0.
-            running_vloss = 0.
+        iou_score = np.empty(len(self.val_loader))
 
+        jaccard = torchmetrics.JaccardIndex(num_classes=151).to(self.device)
+        
+        self.model.eval()
+        with torch.no_grad():
             for i, data in enumerate(self.val_loader):
                 inps, labels_cpu = data
 
                 inps = inps.to(self.device)
-                labels = labels_cpu.to(self.device)
-                
+                masks = labels_cpu.to(self.device)
+
                 outputs = self.model(inps)
-                
-                loss = self.loss_function(outputs, labels)
+
+                loss = self.loss_function(outputs, masks)
                 running_loss += loss.item()
                 
+                # IoU score
+                val_iou = jaccard(outputs, masks).cpu().numpy()
+                iou_score[i] = val_iou  
+                    
         avg_loss = running_loss / (i + 1)
         self.avg_loss_valid.append(avg_loss)
+        self.iou_valid.append(np.mean(iou_score))
         
         return avg_loss
     
-    
+    @torch.no_grad()
     def val_classification_one_epoch(self, action: str='validation'):
-        '''
-        param action: validation or testing
-        '''
+        """
+        Parameters
+        ----------
+        action:  str
+            Validation or testing
+        """
         loader = self.val_loader
         if action == 'testing':
             loader = self.test_loader
             
         running_loss = 0.
-        self.model.train(False)
-        with torch.no_grad():
-            total = 0.
-            correct = 0.
-            running_vloss = 0.
+        self.model.eval()
+        total = 0.
+        correct = 0.
+        running_vloss = 0.
 
-            for i, data in enumerate(loader):
-                inps, labels_cpu = data
+        for i, data in enumerate(loader):
+            inps, labels_cpu = data
 
-                inps = inps.to(self.device)
-                labels = labels_cpu.to(self.device)
+            inps = inps.to(self.device)
+            labels = labels_cpu.to(self.device)
                 
-                outputs = self.model(inps)
+            outputs = self.model(inps)
                 
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
                 
-                if action == 'validation':
-                    loss = self.loss_function(outputs, labels)
-                    running_loss += loss.item()
+            if action == 'validation':
+                loss = self.loss_function(outputs, labels)
+                running_loss += loss.item()
         
         # calculate f1 score for one epoch  
         f1score = f1_score(labels.cpu(), predicted.cpu(), average='weighted')
@@ -137,39 +148,16 @@ class Train:
             return temp_accuracy, avg_loss
     
         return temp_accuracy, f1score
-
-    
-    def plot_losses(self, train_losses: list, valid_losses: list,
-                    fig_size_width: int, fig_size_height: int):
-        
-        plt.figure(figsize=(fig_size_width, fig_size_height))
-        plt.plot(train_losses, label='Train')
-        plt.plot(valid_losses, label='Validation')
-        plt.legend()
-        plt.title('Train and validation losses')
-        plt.xlabel('Epoch number')
-        plt.ylabel('Loss')
-        plt.grid(True)
-        plt.show()
-    
-    def plot_accuracy(self, validation_accuracy: list,
-                      fig_size_width: int, fig_size_height: int):
-        
-        plt.figure(figsize=(fig_size_width, fig_size_height))
-        plt.plot(validation_accuracy, label='Validation')
-        plt.legend()
-        plt.title('Validation accuracy')
-        plt.xlabel('Epoch number')
-        plt.ylabel('Accuracy')
-        plt.grid(True)
-        plt.show()
         
     def train_procedure(self, task='classification'):
-        '''
-        param: task: if you solve semantic segmentation task, you don't need to
-        calculate accuracy and f1 score in train_procedure (task=segmentation)
-        '''
-        
+        """
+        Parameters
+        ----------
+        task: str
+            Classification or segmentation
+            metrics: accuracy and f1 score for classification.
+                     IoU score for segmentation.
+        """
         if task == 'classification':
             
             for epoch in range(self.num_epochs):
@@ -191,47 +179,64 @@ class Train:
                 avg_loss = self.training_one_epoch()
                 # VALIDATION
                 avg_vloss = self.val_segmentation_one_epoch()
-           
-
-    def reverse_transform(self, inp):
-        inp = inp.numpy().transpose((1, 2, 0))
-        mean = np.array([0.485, 0.456, 0.406])
-        std = np.array([0.229, 0.224, 0.225])
-        inp = std * inp + mean
-        inp = np.clip(inp, 0, 1)
-        inp = (inp * 255).astype(np.uint8)
-
-        return inp
     
-    def predict_segmentation(self, idx):
-        '''
-        Predict image mask for semantic segmentation task.
-        param idx: plt.show image, its true mask and predicted mask by idx
-        '''
-
-        self.model.eval()   # Set model to evaluate mode
+    def predict_segmentation(self, idx, fig_widtht=10, fig_height=10):
+        """ Predict image mask for semantic segmentation task.
+        
+        Parameters
+        ----------
+        idx: int
+            plt.show image, its true mask and predicted mask by idx
+        """
+        self.model.eval()   
 
         inputs, labels = next(iter(self.test_loader))
         inputs = inputs.to(self.device)
         labels = labels.to(self.device)
 
-        pred = self.model(inputs)
-        pred = torch.sigmoid(pred)
-        pred = pred.data.cpu().numpy()
+        outputs = self.model(inputs)
+        
+        predicted = torch.argmax(outputs, axis=1)
+        target_masks = labels.cpu().numpy()
+        predicted = predicted.cpu().detach().numpy()
+        
+        for i in range(idx, idx + 3):
+            fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(fig_widtht, fig_height))
 
-        # Change channel-order and make 3 channels for matplot
-        input_images_rgb = [self.reverse_transform(x) for x in inputs.cpu()]
+            ax1.imshow(inputs[i].permute(1, 2, 0).cpu().numpy())
+            ax1.set_title('source image')
 
-        target_masks_rgb = labels.cpu().numpy()
+            ax2.imshow(target_masks[i])
+            ax2.set_title('ground truth')
 
-        plt.imshow(input_images_rgb[idx])
-        plt.title('source image')
+            ax3.imshow(predicted[i])
+            ax3.set_title('predicted mask')
+            plt.show()
+            
+    
+    def plot_losses(self, train_losses: list, valid_losses: list,
+                    fig_size_width: int, fig_size_height: int):
+        
+        plt.figure(figsize=(fig_size_width, fig_size_height))
+        plt.plot(train_losses, label='Train')
+        plt.plot(valid_losses, label='Validation')
+        plt.legend()
+        plt.title('Train and validation losses')
+        plt.xlabel('Epoch number')
+        plt.ylabel('Loss')
+        plt.grid(True)
         plt.show()
-        plt.imshow(target_masks_rgb[idx])
-        plt.title('ground truth')
-        plt.show()
-        plt.imshow(np.squeeze(pred[0][idx]))
-        plt.title('predicted mask')
+    
+    def plot_metric(self, validation_accuracy: list,
+                      fig_size_width: int, fig_size_height: int, metric_name='Accuracy'):
+        
+        plt.figure(figsize=(fig_size_width, fig_size_height))
+        plt.plot(validation_accuracy, label='Validation')
+        plt.legend()
+        plt.title(f'Validation {metric_name.lower()}')
+        plt.xlabel('Epoch number')
+        plt.ylabel(f'{metric_name}')
+        plt.grid(True)
         plt.show()
     
     def show_sample(self, img, target):
@@ -248,7 +253,6 @@ class Train:
         self.show_sample(img, target=self.test_dataset.classes[preds[0].item()])
         
     def plot_confusion_matrix(self, fig_size_width=10, fig_size_height=8):
-        
         predictions = np.array([0])
         labels_true = np.array([0])
 
